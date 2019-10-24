907ca8915eba98a92b9d2dded78d9417762388dd
NoxZet <nicolas.m@email.cz>
2019-10-25 00:46:21 +0200

    Adding WinAPI main for simple rendering

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <chrono>
 #include <thread>
+#include <windows.h>
 
 #include "main.h"
 #include "cpu.h"
@@ -11,6 +12,89 @@
 using namespace NoxGB;
 using namespace std::chrono;
 using namespace std::this_thread;
+
+#define UNICODE
+
+static unsigned char* frameBuffer;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+	// Register the window class.
+	const char CLASS_NAME[] = "GBEmu";
+
+	WNDCLASS wc = {};
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	// Create the window.
+
+	HWND hwnd = CreateWindowEx(
+		0,                              // Optional window styles.
+		CLASS_NAME,                     // Window class
+		"GBEmu",    // Window text
+		WS_OVERLAPPEDWINDOW,            // Window style
+
+										// Size and position
+		CW_USEDEFAULT, CW_USEDEFAULT, 400, 400,
+
+		NULL,       // Parent window    
+		NULL,       // Menu
+		hInstance,  // Instance handle
+		NULL        // Additional application data
+	);
+
+	if (hwnd == NULL)
+	{
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+}
+
+void printBuffer(HWND hwnd, unsigned char* buffer, size_t width, size_t height) {
+
+	HBITMAP map = CreateBitmap(width, height, 1, sizeof(char) * 4, (void*)buffer);
+	HDC src = CreateCompatibleDC(GetDC(hwnd));
+	SelectObject(src, map);
+	BitBlt(GetDC(hwnd), 10, 10, (int)width, (int)height, src, 0, 0, SRCCOPY);
+	DeleteObject(map);
+	DeleteDC(src); // Deleting temp HD
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hwnd, &ps);
+		printBuffer(hwnd, frameBuffer, 160, 144);
+		EndPaint(hwnd, &ps);
+	}
+	return 0;
+
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 
 int main() {
 	Memory memory;
@@ -30,7 +114,7 @@ int main() {
 	while (true) {
 		frames++;
 		if (frames % 60 == 0)
-		printf("Frame: %d\n", frames);
+			printf("Frame: %d\n", frames);
 		int cycles = 0;
 		int refreshes = gpu.refreshes;
 		bool wasDisabled = !gpu.enable;
@@ -39,7 +123,7 @@ int main() {
 		while (
 			(!wasDisabled && refreshes == gpu.refreshes) ||
 			(wasDisabled && cycles < CYCLES_PER_LINE * 144)
-		) {
+			) {
 			cycles += cpu.runInstruction() + 1;
 			if (!gpu.enable) {
 				wasDisabled = true;
